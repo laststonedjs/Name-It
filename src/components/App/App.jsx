00d1ab5300fbe2b63768e92036455ada7d07f267
Filip <filipsarovic428@gmail.com>
2021-07-11 00:15:40 +0200

    "name-it-react-jsx"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import Header from '../Header/Header';
+import SearchBox from '../SearchBox/SearchBox';
+import ResultsContainer from '../ResultsContainer/ResultsContainer';
 import './App.css';
 
+
+const name = require('@rstacruz/startup-name-generator');
+
 class App extends React.Component {
+    constructor () {
+        super();
+
+        this.state = {
+            headerText: 'Name It !',
+            headerExpanded: true,
+            suggestedNames: [],
+        };
+    }
+
+    handleInputChange = (inputText) => {
+       this.setState({   headerExpanded: !inputText, 
+                            suggestedNames: name(inputText),
+                        });
+    };
+
      render () {
+        
+
         return (
             <div>
-                <Header />
+                <Header 
+                headerExpanded={this.state.headerExpanded}
+                 headTitle={this.state.headerText} 
+                    />
+                <SearchBox onInputChange={this.handleInputChange} />
+                <ResultsContainer suggestedNames={this.state.suggestedNames} />
             </div>
         )
     }
